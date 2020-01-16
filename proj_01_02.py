6b7d7c21eec7c5f246bb519f4576075aa0456ac9
mars-wilson <mlandis@warren-wilson.edu>
2020-01-16 15:56:06 -0500

    asdfasdfasdf

--- proj_01_02.py
+++ proj_01_02.py
@@ -8,6 +8,21 @@ Lab set 01 - Project 01 - Re-acquainting with Python
 # First let's try reading in and displaying the sonnets.
 
 # Read in the file for all the sonnets along with the header and footer garbage
+filename = "wssnt10.txt"
+
+with open(filename) as f:
+    content = f.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+
+
+content = [x.strip() for x in content]   # list comprehension !!!
+
+
+# equivalent to...
+
+content = []
+for x in content:
+    content.append( x.strip() )
 
 # print the number of lines in the file
 

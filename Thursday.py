4715c106523f7c51e63fb707c175781e9b2e28e6
siennawire <sewire1234@gmail.com>
2020-01-30 16:00:59 -0500

    chain hang low

---
+++ Thursday.py
@@ -0,0 +1,12 @@
+"""
+1) Regular Expressions
+Look at proj_01_08.py.  If you execute that in the console, it defines some variables.
+Put a multiline comment in your Thursday.py for this section
+Copy paste these instructions in there.  (copy out everything highlighted)
+Look through the "links" variable and see if you can find some image covers.
+Edit Proj_01_08 to make the loop in proj_01_08 print out the path to the images for all of the book cover thumb images.
+
+"""
+
+links = soup.find_all("a", class_="link")
+
